2c19e4b21907af28e8b0d850b8fb5356b34d8fad
Humberto Massa <humbertomassa@gmail.com>
2025-08-11 20:43:43 -0300

    fix: 🩹 github-ci

--- wfv.cc
+++ wfv.cc
@@ -6,35 +6,33 @@
 #include <algorithm>
 #include <string_view>
 
-#include "../portable-executable-library/pe_lib/pe_bliss.h"
-#include "../portable-executable-library/pe_lib/pe_bliss_resources.h"
+#include "pe_bliss.h"
+#include "pe_bliss_resources.h"
 
 using namespace pe_bliss;
 
 auto windows_file_version(std::string_view f) -> std::string {
-    std::ifstream input(f.data(), std::ios::in | std::ios::binary );
-    pe_base b = pe_factory::create_pe(input);
-    const resource_directory dir = get_resources(b);
-    pe_resource_viewer v { dir };
-    lang_string_values_map strings;
-    translation_values_map translations;
-    file_version_info i { resource_version_info_reader(v).get_version_info(strings, translations) };
-    return i.get_file_version_string<char>();
+    try {
+        std::ifstream input { f.data(), std::ios::in | std::ios::binary };
+        pe_base b { pe_factory::create_pe(input) };
+        const resource_directory dir { get_resources(b) };
+        pe_resource_viewer v { dir };
+        lang_string_values_map strings;
+        translation_values_map translations;
+        file_version_info i { resource_version_info_reader(v).get_version_info(strings, translations) };
+        return i.get_file_version_string<char>();
+    } catch(...) {
+        return {};
+    }
 }
 
 int main(int argc, char **argv) {
-  if( argc == 2 ) {
-    try {
-      std::cout << windows_file_version(argv[1]) << '\n';
-    } catch(...) {
+    if( argc == 2 ) {
+        std::cout << windows_file_version(argv[1]) << '\n';
+        return 0;
     }
-    return 0;
-  }
-  for( std::string_view a: std::span { argv+1, argv+argc } ) {
-    try {
-      std::cout << windows_file_version(a) << '\t' << a << '\n';
-    } catch(...) {
+    for( std::string_view a: std::span { argv+1, argv+argc } ) {
+        std::cout << windows_file_version(a) << '\t' << a << '\n';
     }
-  }
 }
 
